3eba08453024287109706c8b63e879a86c05dc7c
GiacomoGitHub <poddagiacomo@gmail.com>
2021-01-25 13:47:09 +0100

    Added buttons in the UI

--- src/index.js
+++ src/index.js
@@ -4,5 +4,6 @@ import tasksFactory from "./tasksFactory.js";
 
 import projectsFactory from "./projectsFactory.js";
 
-const project1 = projectsFactory("Very important project", "Description");
+const project1 = projectsFactory("Create the todo app", "This is an exercise from The Odin Project");
 const task1 = tasksFactory("Do this task first", "Description", "Due date", "Priority", "Notes");
+
