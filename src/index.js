edd2375efafe72e71dde04dad0ddff489eb8d55b
GiacomoGitHub <poddagiacomo@gmail.com>
2021-01-28 19:09:57 +0100

    Added projects and tasks creation feature

--- src/index.js
+++ src/index.js
@@ -1,9 +1,28 @@
+// import all modules
 import css from "./stylesheet.css";
 
 import tasksFactory from "./tasksFactory.js";
 
 import projectsFactory from "./projectsFactory.js";
 
-const project1 = projectsFactory("Create the todo app", "This is an exercise from The Odin Project");
-const task1 = tasksFactory("Do this task first", "Description", "Due date", "Priority", "Notes");
+// Add functionalities to new project button
+const projectsButtonTargeter = () => {
+    document.getElementById("projectsButton").addEventListener('click', () => {
+        let newProject = document.createElement("H2");
+        let textProject = document.createTextNode("Hello, World!");
+        newProject.appendChild(textProject);
+        document.getElementById("projectsList").appendChild(newProject);
+    });
+};
+projectsButtonTargeter();
 
+// Add functionalities to new task button
+const tasksButtonTargeter = () => {
+    document.getElementById("tasksButton").addEventListener('click', () => {
+            let newTask = document.createElement("H2");
+            let textTask = document.createTextNode("Hello, World!");
+            newTask.appendChild(textTask);
+            document.getElementById("tasksList").appendChild(newTask);
+    });
+};
+tasksButtonTargeter();
